cea32a5e90d174be93ea259609bf1d5211f8f652
ivongr <itzelivongr@gmail.com>
2024-10-21 16:37:09 -0600

    feat: agrega archivo para test del Hook counter.ts

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import useCount from './counter';
 
 
 function App() {
-  const {count, Increment, Decrease,handleClick} = useCount(0);
-  const  handleValueParse= (e) => {
-  const valueNum = parseInt(e.target.value);
-  handleClick(valueNum)
-    }
+  const { count, Increment, Decrease, handleClick } = useCount(0);
+  const handleValueParse = (e) => {
+    const valueNum = parseInt(e.target.value);
+    handleClick(valueNum)
+  }
   return (
     <>
       <input type="number" value={parseInt(count)}
-      onChange={handleValueParse}></input>
+        onChange={handleValueParse}></input>
       <h1>Contador {count}</h1>
       <button className="success" onClick={Increment}>Incrementar</button>
       <button className="danger" onClick={Decrease} >Disminuir</button>
