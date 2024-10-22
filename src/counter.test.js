7c9c8d4c57796ed724b5a346ba3d514af0a0d1a7
ivongr <itzelivongr@gmail.com>
2024-10-21 21:21:37 -0600

    chore: solucionar error de la libreria

--- src/counter.test.js
+++ src/counter.test.js
@@ -1,27 +1,22 @@
-import React from 'react';
-import { expect } from 'chai';
-import { render } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react' 
+import useCount from "./counter";
 
-import useCount from './counter';
-
-test('debe incrementar el contador', () => {
-    const initialCount = 0;
+test("debe incrementar el contador",()=>{
+    let initialCount = 0
     const { result } = renderHook(() => useCount(initialCount));
-
     act(() => {
         result.current.Increment();
     });
-
     expect(result.current.count).toBe(1);
 });
 
-test('debe disminuir el contador', () => {
-    const initialCount = 0;
-    const { result } = renderHook(() => useCount(initialCount));
-
+test("debe de disminuir el contador",() =>{
+    const initialCount = 0; // establece el valor inicial
+    const {result } = renderHook (() => useCount(initialCount)); // pasa el valor inicial
     act(() => {
         result.current.Decrease();
     });
-
     expect(result.current.count).toBe(-1);
-});
+})
+
+
