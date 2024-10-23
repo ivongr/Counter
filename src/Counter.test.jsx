29c9fd02253c81fabf6d347fb78874bcafbadeeb
ivongr <itzelivongr@gmail.com>
2024-10-22 23:19:12 -0600

    update: actualiza test del componente App

--- src/Counter.test.jsx
+++ src/Counter.test.jsx
@@ -1,37 +1,57 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import useCount from './counter';
+import '@testing-library/jest-dom'; // Asegúrate de importar esto
 import App from './App';
+import { expect } from 'vitest';
 
 describe("App Componente", () => {
     let numberInput;
     let counterInit;
-    let counterUpdate;
     let incrementButton;
     let decreaseButton;
 
     beforeEach(() => {
         render(<App />);
         numberInput = screen.getByLabelText("Ingresa un número");
-        counterInit = parseInt(screen.getByRole("count-indicator").innerText)
-        counterUpdate = parseInt(screen.getByRole("count-indicator").innerText)
         incrementButton = screen.getByText("Incrementar");
         decreaseButton = screen.getByText("Disminuir");
+        counterInit = screen.getByRole("count-indicator");
     });
 
     test("el contador empieze desde 0", () => {
         //let conter = parseInt(counterInit)
-        expect(counterInit.textContent).toBe("0")
+        expect(counterInit).toHaveTextContent("0")
     })
     test("incrementa cuando da click", () => {
-        fireEvent.change(numberInput, { target: { value: 5 } })
+        // fireEvent.change(numberInput, { target: { value: 5 } })
         //expect(numberInput).toHaveBeenCalledWith(5)
         fireEvent.click(incrementButton);
-        expect(counterUpdate).toBe(counterInit + 1)
+        const counterUpdate = screen.getByRole("count-indicator");
+        expect(counterUpdate).toHaveTextContent("1")
     });
 
     test("disminuye cuando da click", () => {
         fireEvent.click(decreaseButton);
-        expect(counterUpdate).toBe(counterInit - 1)
+        const counterUpdate = screen.getByRole("count-indicator");
+        expect(counterUpdate).toHaveTextContent("-1")
     });
+
+    test("el contador comienza con el valor inicial establecido por el usuario", () => {
+        fireEvent.change(numberInput, { target: { value: '10' } });
+        fireEvent.click(incrementButton);
+        const counterUpdate = screen.getByRole("count-indicator");
+        expect(counterUpdate).toHaveTextContent("11");
+    });
+
+    test("el contador comienza con el valor inicial establecido por el usuario", () => {
+        fireEvent.change(numberInput, { target: { value: '10' } });
+        fireEvent.click(decreaseButton);
+        const counterUpdate = screen.getByRole("count-indicator");
+        expect(counterUpdate).toHaveTextContent("9");
+    });
+
+    test("validar estilos", () => {
+        expect(incrementButton).toHaveClass("success")
+    })
+
 });
 
