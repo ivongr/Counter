29986575cf9b40597da56f39e31caff86f85867a
ivongr <itzelivongr@gmail.com>
2024-10-23 22:52:13 -0600

    fix: corregir manejo de valores en el hook useCounter.test.js

--- src/useCounter.test.js
+++ src/useCounter.test.js
@@ -2,13 +2,12 @@ import { renderHook, act } from '@testing-library/react'
 import useCount from './counter';
 
 
-
 describe("Counter Hook", () => {
     const initialCount = 0;
-   
-    test("debe incrementar el contador", () => {
-        const { result } = renderHook(() => useCount(initialCount));
 
+    test("debe incrementar el contador", () => {
+
+        const { result } = renderHook(() => useCount(initialCount));
         act(() => {
             result.current.increment();
         });
@@ -16,11 +15,23 @@ describe("Counter Hook", () => {
     });
 
     test("debe de disminuir el contador", () => {
+
         const { result } = renderHook(() => useCount(initialCount));
         act(() => {
             result.current.decrease();
         });
         expect(result.current.count).toBe(-1);
+    });
+
+    
+    test("debe establecer el contador al valor ingresado por el usuario", () => {
+
+        const initialCount = 5;
+        const { result } = renderHook(() => useCount(initialCount));
+        act(() => {
+            result.current.handleClick(initialCount);
+        });
+        expect(result.current.count).toBe(5);
     });
 });
 
