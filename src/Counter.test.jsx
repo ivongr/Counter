0b37285826ecb565d912e94a2ed6c6c9f17d2041
ivongr <itzelivongr@gmail.com>
2024-10-23 22:40:26 -0600

    update: corrige el error del numberInput
    solo faltaba agregar el await

--- src/Counter.test.jsx
+++ src/Counter.test.jsx
@@ -42,14 +42,14 @@ describe("App Componente", () => {
 
     test("debe de comenzar el contador con el valor inicial establecido por el usuario e incrementar el contador", async () => {
         const { numberInput, incrementButton, counterInit, user } = setup();
-        user.type(numberInput, "10");
+        await user.type(numberInput, "10");
        await user.click(incrementButton);
         expect(counterInit.textContent).toBe("11");
     });
 
     test("debe de comenzar el contador con el valor inicial establecido por el usuario e disminuir el contador", async () => {
         const { numberInput, decreaseButton, counterInit, user } = setup();
-        user.type(numberInput, "11");
+       await user.type(numberInput, "11");
        await user.click(decreaseButton);
         expect(counterInit.textContent.trim()).toBe("10");
     });
